94f8f62bb865fa8e2e2ffb375253de00cbfd705b
Sahildhargave <sahildhargave5253.com>
2024-04-03 17:34:47 +0530

    setup handler file

--- account/handler/handler.go
+++ account/handler/handler.go
@@ -19,6 +19,14 @@ func NewHandler(c *config) {
 	g := c.R.Group("ACCOUNT_API_URL")
 
 	g.GET("/me", h.Me)
+	g.POST("/signup", h.Signup)
+	g.POST("/signin", h.Signin)
+	g.POST("/signout", h.Signout)
+	g.POST("/tokens", h.Tokens)
+	g.POST("/image", h.Image)
+	g.DELETE("/image", h.DeleteImage)
+	g.PUT("/details", h.Details)
+
 	// signup
 	//signin
 	//signout
@@ -31,5 +39,47 @@ func NewHandler(c *config) {
 func (h *Handler) Me(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"hello": "it's me",
+	})
+}
+
+func (h *Handler) Signup(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "it's signup",
+	})
+}
+
+func (h *Handler) Signin(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "it's signin",
+	})
+}
+
+func (h *Handler) Signout(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "it's signout",
+	})
+}
+
+func (h *Handler) Tokens(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "it's tokens",
+	})
+}
+
+func (h *Handler) Image(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "it's image",
+	})
+}
+
+func (h *Handler) DeleteImage(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "it's Delete Image",
+	})
+}
+
+func (h *Handler) Details(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "it's Details",
 	})
 }
